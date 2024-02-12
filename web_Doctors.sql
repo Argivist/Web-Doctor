a7b34ac8663fda93acf86de2165d1808071eb6bf
Asfaam <faisalidu.asfaan1@gmail.com>
2024-02-12 17:48:15 +0000

    .

--- web_Doctors.sql
+++ web_Doctors.sql
@@ -4,7 +4,30 @@ CREATE DATABASE web_doctors;
 
 USE web_doctors;
 
-CREATE TABLE doctors (docId INT PRIMARY KEY, docName VARCHAR(100));
+CREATE TABLE Pharmacy (
+	pharmID INT AUTO_INCREMENT PRIMARY KEY,
+	pharmName VARCHAR(50) NOT NULL,
+	pharmInventory INT NOT NULL
+ );
 
-CREATE TABLE Pharmacist (pharmId INT PRIMARY KEY, pharmName VARCHAR(50));
+CREATE TABLE Customer (
+	customID INT AUTO_INCREMENT PRIMARY KEY,
+	customName VARCHAR(50) NOT NULL
+ );
+ 
+ CREATE TABLE Medicine_inventory (
+	medicineID INT AUTO_INCREMENT PRIMARY KEY,
+    	amount DECIMAL(13,2) NOT NULL,
+    	pharmID INT,
+	FOREIGN KEY (pharmID) REFERENCES Pharmacy(pharmID)
+);
+
+CREATE TABLE Prescription (
+	prescriptionID INT AUTO_INCREMENT PRIMARY KEY, 
+	customID INT,
+    	medicineID INT,
+    	FOREIGN KEY (customID) REFERENCES Customer(customID),
+    	FOREIGN KEY (medicineID) REFERENCES Medicine_inventory(medicineID)
+ );
+
 
