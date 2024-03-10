c83b04d5f35d4e45b119989864c0742ea91aa010
hilaryookrah <131860592+hilaryookrah@users.noreply.github.com>
2024-03-10 10:54:20 -0700

    Core php to check if user is logged in

---
+++ settings/core.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+function login_user(){
+    if (isset($_SESSION['userID'])) 
+    {  
+        return true;
+    }
+    else{
+        header( "Location: ../login/login.php" );
+        die();
+    }
+}
+?>
