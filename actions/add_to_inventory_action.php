060e9d717d0464671019d8d3aa2f7e77f105ca3f
hilaryookrah <131860592+hilaryookrah@users.noreply.github.com>
2024-03-17 07:03:12 -0700

    query update

--- actions/add_to_inventory_action.php
+++ actions/add_to_inventory_action.php
@@ -2,11 +2,11 @@
     include '../settings/connection.php';
 
     if (isset($_POST["addBtn"])) {
-        $productID = $_POST['prodID'];
+        // $productID = $_POST['prodID'];
         $productName = $_POST['prodName'];
         $productQuantity = $_POST['quantity'];
         
-        $store = "INSERT INTO Inventory() VALUES ('$productID','$productName','$productQuantity')";
+        $store = "INSERT INTO medicine_inventory(medicine_name,medicine_qty) VALUES ('$productName','$productQuantity')";
     
         if (mysqli_query($conn, $store)) {
             header("Location: ../admin/Inventory.php");
